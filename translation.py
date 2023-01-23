8aa46324267b15feb83c5a08b73b9a7e1f81c01f
monika359b <89681654+monika359b@users.noreply.github.com>
2023-01-23 09:30:09 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,7 +1,13 @@
 class Translation(object):
-    START_TEXT = """Hi I'm A Simple My.telegram.org Bot.
-\n\n
-Help You To Get API ID \n\n /start the bot again to Scrap your app id and hash"""
+    START_TEXT = """
+Hi I'm A Simple My.telegram.org Bot. Help You To Get API ID & API HASH Enter Your Telegram Phone Number With Country Code. Click /start to start the bot"""
+    AFTER_RECVD_CODE_TEXT = """
+Now please send the Telegram code you received from Telegram!
+
+this code is only used for the purpose of getting the APP ID from my.telegram.org
+\nIf you don't trust the developer of this bot, please visit telegram trusted web my.telegram.org
+
+Click /start to restart the bot """
     BEFORE_SUCC_LOGIN = "Please wait \n We are Scrapping for you"
     ERRED_PAGE = "Failed To Get Api Id\nPlease get it manually from my.telegram.org login by phone number there > Enter otp received in telegram app> choose Api Development tool > copy app id and ApI hash>done"
     CANCELLED_MESG = "bye! Please /start again to restart the Bot "
